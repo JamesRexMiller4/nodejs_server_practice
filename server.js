a5aec1ef469c027df3b279ec45d43aa069eb7bfd
James Miller <james.rex.miller.4@gmail.com>
2020-01-25 14:07:23 -0700

    Add getAllMessages and addMessage functions to node server

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer();
+
+let messages = [
+  { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
+  { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
+  { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
+];
+
+const getAllMessages = (res) => {
+  res.writeHead(200, {
+    'Content-Type': 'application/json'
+  })
+  res.end(JSON.stringify(messages))
+}
+
+const addMessage = (newMessage, res) => {
+  messages.push(newMessage)
+  res.writeHead(201, {
+    'Content-Type': 'application/json'
+  })
+  res.end(JSON.stringify(newMessage))
+}
+
+server.listen(3000, () => {
+  console.log('The HTTP server is listening at Port 3000.');
+});
+
+server.on('request', (req, res) => {
+
+  if (req.method === 'GET') {
+    getAllMessages(res)
+  }
+
+  else if (req.method === 'POST') {
+    let newMessage = { 'id': new Date() };
+
+    req.on('data', (data) => {
+      newMessage = Object.assign(newMessage, JSON.parse(data));
+    });
+
+    req.on('end', () => {
+      addMessage(newMessage, res);
+    });
+  }
+});
